994fd45453fd52626e1e3af6db4a87703430a629
AmitCoding62 <sivanandprajapti1234sivawww@gmail.com>
2024-06-20 18:22:37 +0530

    Basic Input System For Binary Tree.

---
+++ BINARY TREE/CREATION OF BINARY TREE/BASIC TREE INPUT SYSTEM/input.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+void BuildFromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    int data;
+    cout << "Enter the Data For Root Node : ";
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        cout << "Enter Left Node Data For : " << temp->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            temp->left = new node(data);
+            q.push(temp->left);
+        }
+        cout << "Enter Right Node Data For : " << temp->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            temp->right = new node(data);
+            q.push(temp->right);
+        }
+    }
+    cout << endl;
+}
+void LevelOrder(node *root)
+{
+    cout << "Printing The Tree : " << endl;
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << "  ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+int main()
+{
+    node *root = NULL;
+    BuildFromLevelOrder(root);
+    LevelOrder(root);
+}
