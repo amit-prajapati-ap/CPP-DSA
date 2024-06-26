1a6ede4d099713ea6ff460ef3abaf5ec9ef403cf
AmitCoding62 <sivanandprajapti1234sivawww@gmail.com>
2024-06-26 20:53:59 +0530

    Program to printing the Top View.

---
+++ BINARY TREE/TOP VIEW/topview.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <queue>
+#include <map>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+void BuildFromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    int data;
+    cout << "Enter the Data For Root Node : ";
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        cout << "Enter Left Node Data For : " << temp->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            temp->left = new node(data);
+            q.push(temp->left);
+        }
+        cout << "Enter Right Node Data For : " << temp->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            temp->right = new node(data);
+            q.push(temp->right);
+        }
+    }
+}
+void LevelOrder(node *root)
+{
+    cout << "Printing The Tree : " << endl;
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << "  ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+vector<int> TopView(node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+
+    map<int, int> topNode;
+    queue<pair<node *, int>> q;
+
+    q.push(make_pair(root, 0));
+
+    while (!q.empty())
+    {
+        pair<node *, int> temp = q.front();
+        q.pop();
+        node *frontNode = temp.first;
+        int hd = temp.second;
+        if (topNode.find(hd) == topNode.end())
+            topNode[hd] = frontNode->data;
+
+        if (frontNode->left)
+            q.push(make_pair(frontNode->left, hd - 1));
+        if (frontNode->right)
+            q.push(make_pair(frontNode->right, hd + 1));
+    }
+
+    for (auto i : topNode)
+    {
+        ans.push_back(i.second);
+    }
+    return ans;
+}
+void print(vector<int> ans)
+{
+    for (int i : ans)
+    {
+        cout << i << "  ";
+    }
+    cout << endl;
+}
+int main()
+{
+    node *root = NULL;
+    BuildFromLevelOrder(root);
+    LevelOrder(root);
+    vector<int> Top = TopView(root);
+    cout << "Printing The Top View Elements : " << endl;
+    print(Top);
+}
