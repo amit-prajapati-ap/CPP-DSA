ac45e83b30d12cd0fbd119513ca098d13737cf09
AmitCoding62 <sivanandprajapti1234sivawww@gmail.com>
2024-07-01 23:24:58 +0530

    Program That Flatten a Binary Tree Into Linked List.

---
+++ BINARY TREE/FLATTEN BT INTO LL/flatten.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+void BuildFromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    int data;
+    cout << "Enter the Data For Root Node : ";
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        cout << "Enter Left Node Data For : " << temp->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            temp->left = new node(data);
+            q.push(temp->left);
+        }
+        cout << "Enter Right Node Data For : " << temp->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            temp->right = new node(data);
+            q.push(temp->right);
+        }
+    }
+}
+void LevelOrder(node *root)
+{
+    cout << "Printing The Tree : " << endl;
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << "  ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+void Flatten(node *root)
+{
+    node *curr = root;
+    while (curr != NULL)
+    {
+        if (curr->left)
+        {
+            node *pred = curr->left;
+            while (pred->right)
+            {
+                pred = pred->right;
+            }
+            pred->right = curr->right;
+            curr->right = curr->left;
+            curr->left = NULL;
+        }
+        curr = curr->right;
+    }
+    // Printing
+    curr = root;
+    while (curr)
+    {
+        cout << curr->data << "  ";
+        curr = curr->right;
+    }
+    cout << endl;
+}
+int main()
+{
+    node *root = NULL;
+    BuildFromLevelOrder(root);
+    LevelOrder(root);
+    cout << "Flatten A Binary tree : " << endl;
+    Flatten(root);
+}
