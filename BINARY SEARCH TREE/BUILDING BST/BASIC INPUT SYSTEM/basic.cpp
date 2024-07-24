fc7cc50161cd8e85ed9171ad09b0b3ad1a4db14e
AmitCoding62 <sivanandprajapti1234sivawww@gmail.com>
2024-07-24 21:45:41 +0530

    Basic Input System For BST.

---
+++ BINARY SEARCH TREE/BUILDING BST/BASIC INPUT SYSTEM/basic.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+// CREATING THE CLASS NODE
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int data)
+    {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+// INSERTION IN BST
+node *insertbst(node *root, int data)
+{
+    if (root == nullptr)
+    {
+        root = new node(data);
+        return root;
+    }
+    if (data > root->data)
+    {
+        root->right = insertbst(root->right, data);
+    }
+    else
+    {
+        root->left = insertbst(root->left, data);
+    }
+    return root;
+}
+int main()
+{
+    // CREATION OF BST
+    node *root = nullptr;
+    cout << "Enter The Data Of BST : ";
+    int data;
+    while (data != -1)
+    {
+        cin >> data;
+        if (data != -1)
+        {
+            root = insertbst(root, data);
+        }
+    }
+}
