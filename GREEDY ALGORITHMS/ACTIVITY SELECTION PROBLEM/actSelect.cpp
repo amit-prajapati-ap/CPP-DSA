0d99cb08ec6ec2be8164caed5c2c3fab245c57bf
AmitCoding62 <sivanandprajapti1234sivawww@gmail.com>
2024-08-22 21:16:29 +0530

    Program That Show a Person can Perform Maximum Activies as per Time.

---
+++ GREEDY ALGORITHMS/ACTIVITY SELECTION PROBLEM/actSelect.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define pair pair<int,int>
+static bool cmp(pair a,pair b) {
+    return a.second < b.second;
+}
+int maxActivities(int n,vector<int> &start,vector<int> &end) {
+    vector<pair> v;
+    for(int i = 0 ; i < n ; i++) {
+        v.push_back({start[i],end[i]});
+    }
+    sort(v.begin(),v.end(),cmp);
+    int count = 1;
+    int ansend = v[0].second;
+    for(int i = 1 ; i < n ; i++) {
+        if(v[i].first > ansend) {
+            count++;
+            ansend = v[i].second;
+        }
+    }
+    return count;
+}
+void input(int &n,vector<int> &start,vector<int> &end) {
+    cout<<"Enter The Number Of Activities : ";
+    cin>>n;
+    cout<<"Enter The Start Time Of Each Activity : "<<endl;
+    for(int i = 0 ; i < n ; i++) {
+        int data;
+        cin>>data;
+        start.push_back(data);
+    }
+    cout<<"Enter The End Time Of Each Activity : "<<endl;
+    for(int i = 0 ; i < n ; i++) {
+        int data;
+        cin>>data;
+        end.push_back(data);
+    }
+}
+void output(int &n,vector<int> &start,vector<int> &end) {
+    cout<<"Printing The Start Time Of Each Activity : "<<endl;
+    for(int i = 0 ; i < n ; i++) {
+        cout<<start[i] << ' ';
+    }
+    cout<<endl;
+    cout<<"Printing The End Time Of Each Activity : "<<endl;
+    for(int i = 0 ; i < n ; i++) {
+        cout<<end[i] << ' ';
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    vector<int> start,end;
+    input(n,start,end);
+    output(n,start,end);
+    cout<<"A Person Can Perform Maximum Activities : "<<maxActivities(n,start,end)<<endl;
+}
