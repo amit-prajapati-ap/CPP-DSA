f57b65172d436d64081dc44622e4656f75b30bd4
AmitCoding62 <sivanandprajapti1234sivawww@gmail.com>
2024-08-22 22:39:02 +0530

    Program To Find Min & Max Of Spending in Candies in the Shop..

---
+++ GREEDY ALGORITHMS/SHOPING IN CANDY STORE/shoping.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+vector<int> shopingCandy(int N, int K, vector<int> &candies)
+{
+    sort(candies, candies + N);
+    int mini = 0;
+    int buy = 0;
+    int freeC = N - 1;
+    while (buy <= freeC)
+    {
+        mini += candies[buy];
+        freeC -= K;
+        buy++;
+    }
+    int maxi = 0;
+    freeC = 0;
+    buy = N - 1;
+    while (freeC <= buy)
+    {
+        maxi += candies[buy];
+        freeC += K;
+        buy--;
+    }
+    vector<int> ans;
+    ans.push_back(mini);
+    ans.push_back(maxi);
+    return ans;
+}
+using namespace std;
+int main()
+{
+    int n, k, data;
+    vector<int> price;
+    cout << "Enter Number of Different Candies : ";
+    cin >> n;
+    cout << "Enter The Number Of Different Candies You Provide For Free : ";
+    cin >> k;
+    cout << "Enter The Price Of Each Type Of Candy : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> data;
+        price.push_back(data);
+    }
+    vector<int> ans = shopingCandy(n, k, price);
+    cout << "Minimum Money Spend to Get All Types Of Candies : " << ans[0] << endl;
+    cout << "Maximum Money Spend to Get All Types Of Candies : " << ans[1] << endl;
+}
